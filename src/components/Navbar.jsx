256b569331e6d8f2ab0528bf13fbc10b5a840967
amiralam1 <amiralam3438@gmail.com>
2025-12-25 17:20:27 +0500

    testing fonts again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,11 @@
-// import React from "react";
-
-// const Navbar = () => {
-//   return (
-//     <div>
-//       <div className="logo bg-slate-300 text-gray-800 kalam-regular text-3xl sm:text-4xl md:text-5xl lg:text-6xl flex justify-center">
-//         <span className="p-4 sm:p-7">
-//           <a href="/">Todo Diary</a>
-//         </span>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
 import React from "react";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="logo bg-slate-300 text-gray-800 text-3xl sm:text-4xl md:text-5xl lg:text-6xl flex justify-center" style={{fontFamily: 'Arial, sans-serif', fontStyle: 'normal'}}>
+      <div className="logo bg-slate-300 text-gray-800 kalam-regular text-3xl sm:text-4xl md:text-5xl lg:text-6xl flex justify-center">
         <span className="p-4 sm:p-7">
-          <a href="/">Todo Diary TEST</a>
+          <a href="/">Todo Diary</a>
         </span>
       </div>
     </div>
